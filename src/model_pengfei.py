761cf86f04a3f7864507c3bafc0add943c2ac4c3
gxlzj <mailgpf@gmail.com>
2016-06-02 18:56:36 -0700

    add some model evaluation stuff

--- src/model_pengfei.py
+++ src/model_pengfei.py
@@ -75,16 +75,18 @@ feature_list.remove('temp')
 feature_list.remove('max temp')
 feature_list.remove('min temp')
 
+#specify model detail
 gbm_model = h2o.estimators.gbm.H2OGradientBoostingEstimator(model_id='test1', distribution='gaussian')
-gbm_model_result = gbm_model.train(y = "temp", x = feature_list, training_frame = train, validation_frame = val)
+#train model
+gbm_model.train(y = "temp", x = feature_list, training_frame = train, validation_frame = val)
+#predict on test data set
+predicted_result = gbm_model.predict(test)
+#calculate mse for both train and valid data
+gbm_model.mse(train=True, valid=True)
+#plot mse error for training data
+gbm_model.plot()
+#report performance of train validation and test ????usage
+gbm_model.model_performance(test_data = test, train = True, valid = True)
+#list of score history
+gbm_model.scoring_history()
 
-
-
-# model.train(feature_list, 'tomorrow_temp', train, validation_frame=val)
-
-# # Determine model performance on test set
-# pred = model.model_performance(test, True, True)
-
-# # Display results
-# print model
-# print pred
